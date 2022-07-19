f72e78b2e4815d59fb7a4b1541408c7cc5342130
0n1dev <all_mind@naver.com>
2022-07-20 00:09:59 +0900

    feat: Jwt 추가 및 Directory 구조 변경

--- src/main/kotlin/io/hodi/teamboapi/domain/user/controller/OAuthController.kt
+++ src/main/kotlin/io/hodi/teamboapi/domain/user/controller/OAuthController.kt
@@ -1,7 +1,9 @@
 package io.hodi.teamboapi.user.controller
 
 import io.hodi.teamboapi.user.service.OAuthService
+import io.hodi.teamboapi.utils.JwtTokenUtils
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -9,11 +11,22 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/oauth")
 class OAuthController(
-    val oAuthService: OAuthService
+    val oAuthService: OAuthService,
+    val jwtTokenUtils: JwtTokenUtils
 ) {
 
     @GetMapping("/google")
     fun authenticate(@RequestParam code: String) {
         println(oAuthService.authenticate(code))
+    }
+
+    @GetMapping
+    fun test(): String? {
+        return jwtTokenUtils.createToken()
+    }
+
+    @GetMapping("/{test}")
+    fun isTest(@PathVariable test: String): String? {
+        return jwtTokenUtils.isValidToken(test).toString()
     }
 }
